9c946a71b4ec1008d2a3f377df251152a2071bb5
Mapal94 <51930145+Mapal94@users.noreply.github.com>
2021-01-14 07:40:31 +0100

    Add files via upload
    Master files for data Type

--- action_blocks.js
+++ action_blocks.js
@@ -4,15 +4,27 @@
 
 
 // Actions (Operations Block in UI)
-//fill pots
-Blockly.Blocks['action_fillpots'] = {
+//boolean action
+Blockly.Blocks['action_boolean'] = {
     init: function() {
       this.jsonInit({
-  "type": "block_type",
-  "message0": "Fill Container %1",
+  "type": "action_boolean",
+  "lastDummyAlign0": "RIGHT",
+  "message0": "Move %1",
   "args0": [
     {
-      "type": "input_dummy"
+      "type": "field_dropdown",
+      "name": "Boolean",
+      "options": [
+        [
+          "True",
+          "OPTIONNAME"
+        ],
+        [
+          "False",
+          "OPTIONNAME"
+        ],
+      ]
     }
   ],
   "inputsInline": true,
@@ -22,16 +34,99 @@ Blockly.Blocks['action_fillpots'] = {
   "tooltip": "",
   "helpUrl": ""
 });
-  }
-};
+    }
+  };
+
+//Numeric action block
+
+Blockly.Blocks['action_numeric'] = {
+    init: function() {
+      this.jsonInit({
+  "type": "action_numeric",
+  "message0": "%1 %2  %3",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "operations_dropdown",
+      "options": [
+        [
+          "<",
+          "Bigger"
+        ],
+        [
+          ">",
+          "Smaller"
+        ],
+        [
+          "=",
+          "Equal"
+        ]
+      ]
+    },
+    {
+      "type": "input_dummy",
+      "align": "CENTRE"
+    },
+    {
+      "type": "input_value",
+      "name": "numeric_value",
+      "align": "RIGHT"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 120,
+  "tooltip": "",
+  "helpUrl": ""
+});
+    }
+  };
+
+//List and arrays actions 
+Blockly.Blocks['action_list'] = {
+    init: function() {
+      this.jsonInit({
+  "type": "action_list",
+  "lastDummyAlign0": "RIGHT",
+  "message0": "%1",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "List",
+      "options": [
+        [
+          "option",
+          "OPTIONNAME"
+        ],
+        [
+          "option",
+          "OPTIONNAME"
+        ],
+        [
+          "option",
+          "OPTIONNAME"
+        ]
+      ]
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 120,
+  "tooltip": "",
+  "helpUrl": ""
+});
+    }
+  };
 
 
-//emergency Stop
-Blockly.Blocks['action_emergencystop'] = {
+//simple action
+Blockly.Blocks['operator_simple_action'] = {
     init: function() {
       this.jsonInit({
   "type": "block_type",
-  "message0": "Stop immediately %1",
+  "message0": "Action 1 %1",
   "args0": [
     {
       "type": "input_dummy"
--- belief_blocks.js
+++ belief_blocks.js
@@ -2,46 +2,13 @@
 'use strict';
 
 
-
-//belief RobotPositionX
- Blockly.Blocks['belief_RobotPositionX'] = {
+//belief boolean
+ Blockly.Blocks['belief_boolean'] = {
     init: function() {
       this.jsonInit({
   "type": "belief_boolean",
   "lastDummyAlign0": "RIGHT",
-  "message0": "Robot ready %1",
-  "args0": [
-    {
-      "type": "field_dropdown",
-      "name": "boolean",
-      "options": [
-        [
-          "True",
-          "OPTIONNAME"
-        ],
-        [
-          "False",
-          "OPTIONNAME"
-        ]
-      ]
-    }
-  ],
-  "inputsInline": true,
-  "output": null,
-  "colour": 230,
-  "tooltip": "",
-  "helpUrl": ""
-});
-    }
-  };
-
-//belief Optical Sensor (is there a cup to fill?)
- Blockly.Blocks['belief_opticalSensorStatus'] = {
-    init: function() {
-      this.jsonInit({
-  "type": "belief_boolean",
-  "lastDummyAlign0": "RIGHT",
-  "message0": "There is a cup to fill %1",
+  "message0": "%1",
   "args0": [
     {
       "type": "field_dropdown",
@@ -68,27 +35,17 @@
   };
 
 
-//belief ConveyorHeadStatus (can I fill another cup or is the line blocked?)
- Blockly.Blocks['belief_conveyorHeadStatus'] = {
+//blief string
+ Blockly.Blocks['belief_string'] = {
     init: function() {
       this.jsonInit({
-  "type": "belief_boolean",
-  "lastDummyAlign0": "RIGHT",
-  "message0": "subsequent line is occupied %1",
+  "type": "belief_string",
+  "message0": "%1",
   "args0": [
     {
-      "type": "field_dropdown",
-      "name": "boolean",
-      "options": [
-        [
-          "True",
-          "OPTIONNAME"
-        ],
-        [
-          "False",
-          "OPTIONNAME"
-        ]
-      ]
+      "type": "input_value",
+      "name": "NAME",
+      "align": "RIGHT"
     }
   ],
   "inputsInline": true,
@@ -99,14 +56,51 @@
 });
     }
   };
-
-
-//belief conveyorSpeed
- Blockly.Blocks['belief_conveyorSpeed'] = {
+  
+  
+//blief list
+ Blockly.Blocks['belief_list'] = {
+    init: function() {
+      this.jsonInit({
+  "type": "belief_list",
+  "lastDummyAlign0": "RIGHT",
+  "message0": "%1",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "List",
+      "options": [
+        [
+          "option",
+          "OPTIONNAME"
+        ],
+        [
+          "option",
+          "OPTIONNAME"
+        ],
+        [
+          "option",
+          "OPTIONNAME"
+        ]
+      ]
+    }
+  ],
+  "inputsInline": true,
+  "output": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+} );
+    }
+  };
+  
+  
+//belief numeric
+ Blockly.Blocks['belief_numeric'] = {
     init: function() {
       this.jsonInit({
   "type": "belief_numeric",
-  "message0": "Conveyor speed %1 %2 %3",
+  "message0": "%1 %2 %3",
   "args0": [
     {
       "type": "field_dropdown",
@@ -144,41 +138,5 @@
 } );
     }
   };
-  
- 
-//blief stackLightStatus
- Blockly.Blocks['belief_stackLightStatus'] = {
-    init: function() {
-      this.jsonInit({
-  "type": "belief_list",
-  "lastDummyAlign0": "RIGHT",
-  "message0": "Stack Light %1",
-  "args0": [
-    {
-      "type": "field_dropdown",
-      "name": "List",
-      "options": [
-        [
-          "red",
-          "OPTIONNAME"
-        ],
-        [
-          "orange",
-          "OPTIONNAME"
-        ],
-        [
-          "green",
-          "OPTIONNAME"
-        ]
-      ]
-    }
-  ],
-  "inputsInline": true,
-  "output": null,
-  "colour": 230,
-  "tooltip": "",
-  "helpUrl": ""
-} );
-    }
-  };
-  
+
+
